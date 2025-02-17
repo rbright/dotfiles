afd7f5216e7e38cd7c52f5f11f22ba451e3f6f2e
Ryan Bright <ryan@moonriseconsulting.io>
2025-02-17 15:51:33 -0700

    feat(nvim): add keymaps for fzf-lua

--- nvim/.config/nvim/lua/config/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/config/plugins/fzf-lua.lua
@@ -8,6 +8,9 @@ require('fzf-lua').setup()
 -- Keymaps
 -------------------------------------------------------------------------------
 
-vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>",
-  function() require("fzf-lua").complete_path() end,
-  { silent = true, desc = "Fuzzy complete path" })
+vim.keymap.set("n", "<leader>,", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", { desc = "Switch Buffer" })
+vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>", { desc = "Buffers" })
+vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Find Files (Root Dir)" })
+vim.keymap.set("n", "<leader>gc", "<cmd>FzfLua git_commits<CR>", { desc = "Commits" })
+vim.keymap.set("n", "<leader>gs", "<cmd>FzfLua git_status<CR>", { desc = "Status" })
+vim.keymap.set("n", "<leader>sg", "<cmd>FzfLua live_grep<cr>", { desc = "Grep (Root Dir)" })
