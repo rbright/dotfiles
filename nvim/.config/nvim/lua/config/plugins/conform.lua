7a673684a76009bc2a5241997dbec7637333a90f
Ryan Bright <ryan@moonriseconsulting.io>
2025-02-17 17:17:56 -0700

    feat(nvim): add conform for auto formatting

---
+++ nvim/.config/nvim/lua/config/plugins/conform.lua
@@ -0,0 +1,22 @@
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "ruff" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", stop_after_first = true },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
